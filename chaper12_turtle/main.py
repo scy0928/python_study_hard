68bd4374ad7378af8312a76f750419abb3bc3ef7
scy0928 <scy090928@naver.com>
2025-02-21 11:45:15 +0900

    feat : 20250221

--- chaper12_turtle/main.py
+++ chaper12_turtle/main.py
@@ -1,46 +1,61 @@
-#turtle이라는 모듈을 사용할 건데, turtle, Screen 클래스를 import할겁니다.
 import random
+
+'''
+chapter12_turtle
+main
+'''
+# turtle이라는 모듈을 사용할건데, Turtle, Screen 클래스를 import 할겁니다
 from turtle import Turtle, Screen
 
-t= Turtle()        #Turtle클래스의 객체 생성, 이름은 t
-screen = Screen()  #Screen 클래스의 객체 생성
+t = Turtle()  # Turtle 클래스의 객체 생성, 이름은 t
+screen = Screen()  # Screen 클래스의 객체 생성
 t.shape("turtle")
 t.color("white")
 screen.bgcolor("black")
+
+
 # t.penup()
 # t.forward(100)
 # t.pendown()
 # t.forward(200)
 
-# for _ in range(10):     #그냥 반복을 10번하는 것도 변수를 사용하지 않았기 때문에 그냥 _를 씀( i나 n이 아니라)
+# for _ in range(10):   # 그냥 반복을 10번 하는 거고 변수를 사용하지 않았기 때문에 _를 썼습니다(i나 n이 아니라)
 #     t.penup()
 #     t.forward(20)
 #     t.pendown()
 #     t.forward(20)
 
+# t.left(90)
+# t.forward(100)
+
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+# t.forward(100)
+# t.left(90)
+
 # for _ in range(4):
 #     t.forward(100)
 #     t.left(90)
-
+#
 # for _ in range(3):
 #     t.forward(100)
 #     t.left(120)
 
-# for _ in range(4):
-#     for _ in range(10):
-#         t.penup()
-#         t.forward(5)
-#         t.pendown()
-#         t.forward(5)
-#     t.left(90)
-# for _ in range(3):
-#     for _ in range(10):
-#         t.penup()
-#         t.forward(5)
-#         t.pendown()
-#         t.forward(5)
-#     t.left(120)
+# for _ in range(10):
+#     t.forward(5)
+#     t.penup()
+#     t.forward(5)
+#     t.pendown()
 
+# 해당 부분까지 다 하신 분들은
+# 오각형, 육각형 ... 십각형까지 그려볼 수 있도록 하고
+# 이후 코드를 축약할 방법에 대해서 고민하도록 하겠습니다
+
+# dotted_line을 그리는 함수를 하나 정의할겁니다
 
 def dotted_line():
     for _ in range(10):
@@ -49,48 +64,151 @@ def dotted_line():
         t.forward(5)
         t.pendown()
 
-# #삼각형도 그려볼까요?
+
+# 삼각형도 그려볼까요
 # for _ in range(3):
 #     dotted_line()
 #     t.left(120)
 #
-# #이상의 함수를 사용하여 사각형을 그린다고 가정했을 때
+# # 이상의 함수를 사용하여 사각형을 그린다고 가정했을 때
 #
 # for _ in range(4):
 #     dotted_line()
 #     t.left(90)
 #
-# #해당 부분까지 다 한신 분들은
-# #오각형, 육각형 ... 십각형까지 그려볼 수 있게 하고
-# #이후 코드를 축약할 방법까지 고민해보도록 하겠습니다.
-#
 # for _ in range(5):
 #     dotted_line()
 #     t.left(72)
 
-
+# 1. draw_figures(num)을 정의하시오.
+# 2. num이 3 미만이라면 "도형을 그릴 수 없습니다"가 출력되고 메서드를 종료하시오.
+# 3. 3이상이라면 해당 메서드가 실행될 수 있도록 하시고,
+# 4. 반복문을 통해 draw_figures를 호출해 삼각형부터 십각형까지 그리는데,
+# 5. 도형마다 색깔이 다를 수 있도록 작성하시오.
 
 def draw_figures(num):
+    t.color(random.choice(colors))
+    if num < 3:
+        print("도형을 그릴 수 없습니다.")
+        return  # 함수에서 return 다음 아무것도 입력하지 않으면 함수 종료
+    for _ in range(num):
+        t.forward(100)
+        t.left(360 / num)
+
+
+def draw_dotted_figures(num):
     for _ in range(num):
         dotted_line()
-        t.left(360/num)
+        t.left(360 / num)
 
-#랜덤 객체를 생성
+
+# 랜덤 객체를 생성
 random = random.Random()
 
+colors = [
+    "dodger blue",
+    "peru",
+    "dark khaki",
+    "sea green",
+    "crimson",
+    "cornsilk",
+    "pale violet red",
+    "dark slate blue",
+    "royal blue",
+    "papaya whip",
+    "khaki",
+    "dark sea green",
+    "CornflowerBlue",
+    "DarkOrchid",
+    "IndianRed",
+    "DeepSkyBlue",
+    "LightSeaGreen",
+    "wheat",
+    "SlateGray",
+    "SeaGreen",
+    "tomato",
+    "dark olive green",
+    "mint cream",
+    "sienna",
+]
 
-colors = ["indian red", "cyan", "medium slate blue", "medium blue", "gold",
-          "medium spring green", "snow", "peru", "dodger blue", "medium blue",
-          "light green", "dark red", "tomato", "chocolate", "lime green"] #내부에 거북이 색깔을 요소로 하는 리스트를 생성
-    #랜덤 모듈을 사용하세요(행맨에서 했습니다)
+t.speed(10)
+# for i in range(3, 11, 1):
+#     t.color(random.choice(colors))
+#     draw_dotted_figures(i)
 
-# t.color(" indian red ")
+# draw_figures(3)
+# draw_figures(4)
+# draw_figures(1)
+
+# for i in range(11):
+# draw_figures(i)
+'''
+    .heading() 메서드 :
+        거북이가 바라보는 방향을 나타내는 속성으로 도(degree) 단위로 나타남.
+
+        해당 메서드는 콘솔창에 float 형태로 출력됩니다.
+        0도 : 동
+        90도 : 북
+        180도 : 서
+        270도 : 남
+
+    .setheading() 메서드 :
+        특정 각도로 거북이를 회전시키는 메서드
+
+    .circle() 메서드 :
+        거북이가 원 그리는 메서드
+'''
 
 
-for i in range(3,11,1):
-    draw_figures(i)
-    t.color(random.choice(colors))
+# t.forward(100)
+# print(t.heading())
+# t.right(90)
+# print(t.heading() * 100)
+
+
+# t.circle(100)
+# t.color(random.choice(colors))
+# t.setheading(t.heading() + 10)
+# t.circle(100)
+# t.color(random.choice(colors))
+# t.setheading(t.heading() + 10)
+# t.circle(100)
+
+# for _ in range(360 // 100):
+#     t.circle(100)
+#     t.color(random.choice(colors))
+#     t.setheading(t.heading() + 100)
+
+# 이상의 네 줄의 코드를 응용하여 draw_spriograph(size_of_gap)로 함수화 하시오.
+
+def draw_spriograph(size_of_gap):
+    for _ in range(360 // size_of_gap):
+        t.circle(100)
+        t.color(random.choice(colors))
+        t.setheading(t.heading() + size_of_gap)
+
+
+t.speed(0)
+draw_spriograph(2)
+
+#
+#
+# for _ in range(3):
+#     for _ in range(10):
+#         t.forward(5)
+#         t.penup()
+#         t.forward(5)
+#         t.pendown()
+#     t.left(120)
+#
+# for _ in range(4):
+#     for _ in range(10):
+#         t.forward(5)
+#         t.penup()
+#         t.forward(5)
+#         t.pendown()
+#     t.left(90)
+
 
 screen.exitonclick()
-
-
---
+++ chapter13_prettytable/main.py
@@ -0,0 +1,18 @@
+'''
+외부 패키지의 설치
+
+좌측 상단 메뉴 버튼(햄버거) ->file -> settings (ctrl +alt +s)
+좌측 리스트에서 project :프로젝트명으로 되어있는 부분 클릭
+->python interpreter
+->좌상단에 + 눌러서 필요한 패키지 검색 및 설치
+'''
+
+from prettytable import PrettyTable
+from pokemon_data import pokemon_data
+
+table = PrettyTable()
+
+table.field_names = ["번호", "이름", "타입"]
+table.add_row([1, "이상해씨", "풀/독"])
+
+print(table)
